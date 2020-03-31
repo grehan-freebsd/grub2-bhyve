9eaf163a4afd65b0b2fc1233f96dc51f6498a0a1
Peter Grehan <grehan@freebsdorg>
2020-03-31 10:12:07 +1000

    Fix build on FreeBSD
    Uncomment redefinition of gets.
    
    Obtained from FreeBSD PR 241168.

--- grub-core/gnulib/stdio.in.h
+++ grub-core/gnulib/stdio.in.h
@@ -141,7 +141,7 @@ _GL_WARN_ON_USE (fflush, "fflush is not always POSIX compliant - "
    so any use of gets warrants an unconditional warning.  Assume it is
    always declared, since it is required by C89.  */
 #undef gets
-_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
+//_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
 
 #if @GNULIB_FOPEN@
 # if @REPLACE_FOPEN@
